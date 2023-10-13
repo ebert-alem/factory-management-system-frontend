aff81d4d1887c90429dc70e7be32dc790420c7ef
Federico Veron <veronfederico45@gmail.com>
2023-10-13 20:21:52 -0300

    added mobile screen fix

--- src/pages/private/materials/Materials.tsx
+++ src/pages/private/materials/Materials.tsx
@@ -1,5 +1,4 @@
 import { Button, Typography } from "@mui/material"
-import "./mateirals.scss"
 import { AddCircleOutlineRounded } from "@mui/icons-material"
 import { DataTableMaterials } from "./components/DataTableMaterial"
 import { AddMaterial } from "."
@@ -16,7 +15,7 @@ export const Materials = () => {
   return (
     <div className="materialTypes">
       <div className="info">
-        <Typography variant="h4">Maleriales</Typography>
+        <Typography variant="h4">Materiales</Typography>
         <Button onClick={() => handlerOpen(true)} variant="text" size="small" color="primary" startIcon={<AddCircleOutlineRounded />}>Nuevo Material</Button>
       </div>
       <ModalMaterial updateMaterials={updateMaterials}/>
